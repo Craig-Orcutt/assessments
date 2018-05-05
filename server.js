c9e9b8a8c1308e6e21a3e9d5d40894240e434045
Craig Orcutt <craigaorcutt@gmail.com>
2018-05-04 22:35:15 -0500

    added res header to fix CORS issue of running on localhost, both of my servers

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const bodyParser = require("body-parser");
 const routes = require("./server/routes");
 
 app.set("models", require("./server/models"));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static(__dirname + "/client"));
 // app.get('/api/hello', (req, res) => {
 //   res.send({ express: 'Hello' });
