0aaa6c4a2eeb86dca3ff905bc06ed013c6118f8b
Craig Orcutt <craigaorcutt@gmail.com>
2018-05-07 15:02:43 -0500

    added fake user and config for auth

--- create-db.js
+++ create-db.js
@@ -12,6 +12,13 @@ models.sequelize
   })
   .then(queryInterface => {
     return models.Client.bulkCreate(clients);
+  })  
+  .then( () => {
+    return models.User.create({
+      username: "bubba",
+      email: "b@b.com",
+      password: "$2a$08$qCnvbxTHBNmNH0hf1kfIiuE6SJcWfMXPAz45enS2pV10tL0vG1mIy"
+    });
   })
   .then(() => {
     process.exit();
