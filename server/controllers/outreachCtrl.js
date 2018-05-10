426227cb3d458fa4b59a4bc5615e5bb8aaf88f1d
Craig Orcutt <craigaorcutt@gmail.com>
2018-05-10 15:42:25 -0500

    assigned new object to be recieved by front-end. includes substances and name of therapist

--- server/controllers/outreachCtrl.js
+++ server/controllers/outreachCtrl.js
@@ -1,11 +1,40 @@
 'use strict';
 
 module.exports.getAllClients = (req,res,next) => {
-  let {Client} = req.app.get('models');
-  Client.findAll({raw:true})
-  .then((data)=>{
-    
-    res.status(200).json(data);
+  let {Client, Substance, User} = req.app.get('models');
+  Client.findAll({include:[Substance, 'therapist']})
+  .then(clients=>{
+    // console.log('clientssss', clients);
+
+    const clientObj = clients.map(client => {
+      return Object.assign(
+        {},
+        {
+          first_name: client.first_name,
+          last_name: client.last_name,
+          gender: client.gender,
+          dob: client.dob,
+          frequency: client.frequency,
+          last_use: client.last_use,
+          length_of_use: client.length_of_use,
+          previous_treatment: client.previous_treatment,
+          mental_health: client.mental_health,
+          si_hi: client.si_hi,
+          progress: client.progress,
+          severity: client.severity,
+          substancesUsed: client.Substances.map(subs => {
+            Object.assign(
+              {},
+              {
+                substance: client.Substances.name
+              }
+            )
+          }),
+          therapist: client.therapist.username
+        }
+      )
+    })
+    res.status(200).json(clientObj);
   })
-  
+
 }
