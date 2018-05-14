0c34ae4d3d9daa7d75e307d86dcc46dfbd412ae0
Craig Orcutt <craigaorcutt@gmail.com>
2018-05-14 14:06:58 -0500

    added routes and controllers to get client data sorted differently

--- server/controllers/outreachCtrl.js
+++ server/controllers/outreachCtrl.js
@@ -4,7 +4,123 @@ module.exports.getAllClients = (req,res,next) => {
   let {Client, Substance, User} = req.app.get('models');
   Client.findAll({include:[Substance, 'therapist']})
   .then(clients=>{
+
+
+    const clientObj = clients.map(client => {
+      return Object.assign(
+        {},
+        {
+          first_name: client.first_name,
+          last_name: client.last_name,
+          gender: client.gender,
+          dob: client.dob,
+          frequency: client.frequency,
+          last_use: client.last_use,
+          length_of_use: client.length_of_use,
+          previous_treatment: client.previous_treatment,
+          mental_health: client.mental_health,
+          si_hi: client.si_hi,
+          progress: client.progress,
+          severity: client.severity,
+          substancesUsed: client.Substances.map(subs => {
+              return subs.name.charAt(0).toUpperCase() + subs.name.slice(1)
+          }),
+          therapist: client.therapist.username,
+          inquiry: client.createdAt
+        }
+      )
+    
+      
+    })
+    
+    res.status(200).json(clientObj);
+  })
+
+}
+
+module.exports.sortBySeverity = (req,res,next) => {
+  let {Client, Substance, User} = req.app.get('models');
+  Client.findAll({include:[Substance, 'therapist'], order:[['severity' , 'DESC']]})
+  .then(clients=>{
     console.log('clientssss', clients);
+
+    const clientObj = clients.map(client => {
+      return Object.assign(
+        {},
+        {
+          first_name: client.first_name,
+          last_name: client.last_name,
+          gender: client.gender,
+          dob: client.dob,
+          frequency: client.frequency,
+          last_use: client.last_use,
+          length_of_use: client.length_of_use,
+          previous_treatment: client.previous_treatment,
+          mental_health: client.mental_health,
+          si_hi: client.si_hi,
+          progress: client.progress,
+          severity: client.severity,
+          substancesUsed: client.Substances.map(subs => {
+              return subs.name.charAt(0).toUpperCase() + subs.name.slice(1)
+          }),
+          therapist: client.therapist.username,
+          inquiry: client.createdAt
+        }
+      )
+    
+      
+    })
+    
+    res.status(200).json(clientObj);
+  })
+
+}
+
+module.exports.sortByGender = (req,res,next) => {
+  let {Client, Substance, User} = req.app.get('models');
+  Client.findAll({include:[Substance, 'therapist'], order:[['gender' , 'DESC']]})
+  .then(clients=>{
+    console.log('clients', clients);
+    
+
+    const clientObj = clients.map(client => {
+      return Object.assign(
+        {},
+        {
+          first_name: client.first_name,
+          last_name: client.last_name,
+          gender: client.gender,
+          dob: client.dob,
+          frequency: client.frequency,
+          last_use: client.last_use,
+          length_of_use: client.length_of_use,
+          previous_treatment: client.previous_treatment,
+          mental_health: client.mental_health,
+          si_hi: client.si_hi,
+          progress: client.progress,
+          severity: client.severity,
+          substancesUsed: client.Substances.map(subs => {
+              return subs.name.charAt(0).toUpperCase() + subs.name.slice(1)
+          }),
+          therapist: client.therapist.username,
+          inquiry: client.createdAt
+        }
+      )
+    
+      
+    })
+    
+    res.status(200).json(clientObj);
+  })
+
+}
+
+module.exports.sortByInquiryDate = (req,res,next) => {
+  let {Client, Substance, User} = req.app.get('models');
+  Client.findAll({include:[Substance, 'therapist'], order:[['createdAt' , 'DESC']]})
+  .then(clients=>{
+    console.log('clients', clients);
+    
 
     const clientObj = clients.map(client => {
       return Object.assign(
--- server/routes/outreach.js
+++ server/routes/outreach.js
@@ -3,8 +3,11 @@
 const { Router } = require("express");
 const router = Router();
 
-const {getAllClients} = require('../controllers/outreachCtrl');
+const {getAllClients, sortBySeverity, sortByGender, sortByInquiryDate} = require('../controllers/outreachCtrl');
 
 router.get("/server/outreach", getAllClients);
+router.get('/server/sortBySeverity', sortBySeverity)
+router.get('/server/sortByGender', sortByGender)
+router.get('/server/sortByInquiryDate', sortByInquiryDate)
 
 module.exports = router;
