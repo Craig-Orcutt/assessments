eb3edc9f21b7bfda4b22c56e1881badcecf2f93c
Craig Orcutt <craigaorcutt@gmail.com>
2018-05-13 21:58:11 -0500

    adjusted routes to clean up file directory

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,61 +1,59 @@
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
-
-import './App.css';
-import Login from './Login';
-import Logout from './Logout';
-import Form from "./Form"
-import Outreach from './Outreach';
+import "./App.css";
+import Login from "./Login";
+import Logout from "./Logout";
+import Form from "../components/Form/Form";
+import Outreach from "../components/Outreach/Outreach";
 
 class App extends Component {
   state = {
-    username: '',
-    userid: '',
+    username: "",
+    userid: "",
     userIsOutreach: false,
-    isHiddenForm : true,
+    isHiddenForm: true,
     isHiddenOutreach: true,
     isHiddenLogin: false,
     isHiddenLogout: true
   };
 
-//  
-setUser = (user) => {
-  console.log('userApp', user);
-  this.setState({
-    username: user.username,
-    userid: user.id,
-    userIsOutreach: user.isOutreach
-  })
-  if(this.state.userIsOutreach !== null){
+  //
+  setUser = user => {
+    console.log("userApp", user);
     this.setState({
-      isHiddenLogin: !this.state.isHiddenLogin,
-      isHiddenLogout: !this.state.isHiddenLogout
-    })
-    if(this.state.userIsOutreach === false){
+      username: user.username,
+      userid: user.id,
+      userIsOutreach: user.isOutreach
+    });
+    if (this.state.userIsOutreach !== null) {
       this.setState({
-        isHiddenForm: !this.state.isHiddenForm
-      })
-    } else if (this.state.userIsOutreach === true) {
-      this.setState({
-        isHiddenOutreach: !this.state.isHiddenOutreach
-      })
-    } else {
-      this.setState({
-        isHiddenForm: true,
-        isHiddenOutreach: true
-      })
+        isHiddenLogin: !this.state.isHiddenLogin,
+        isHiddenLogout: !this.state.isHiddenLogout
+      });
+      if (this.state.userIsOutreach === false) {
+        this.setState({
+          isHiddenForm: !this.state.isHiddenForm
+        });
+      } else if (this.state.userIsOutreach === true) {
+        this.setState({
+          isHiddenOutreach: !this.state.isHiddenOutreach
+        });
+      } else {
+        this.setState({
+          isHiddenForm: true,
+          isHiddenOutreach: true
+        });
+      }
     }
-  }
-  
-  
-}
+  };
 
   render() {
-    
     return (
       <div className="App">
-        {!this.state.isHiddenLogout && <Logout /> }
-        {!this.state.isHiddenLogin && <Login setUser={this.setUser} history={this.history}/>}
+        {!this.state.isHiddenLogout && <Logout />}
+        {!this.state.isHiddenLogin && (
+          <Login setUser={this.setUser} history={this.history} />
+        )}
         {!this.state.isHiddenForm && <Form setUser={this.state.userid} />}
         {!this.state.isHiddenOutreach && <Outreach />}
       </div>
--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Register from "./Register";
-import Form from './Form'
-import Outreach from "./Outreach";
 import NotFound from "./NotFound";
 
 const Router = () => (
@@ -11,8 +9,6 @@ const Router = () => (
     <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/register" component={Register} />
-        <Route exact path="/Form" component={Form} />
-        <Route exact path="/outreach" component={Outreach} />
         <Route component={NotFound} />
     </Switch>
 </BrowserRouter>
