8d5979e5cc2a4c8d17fd05f910765334ad3defe7
Craig Orcutt <craigaorcutt@gmail.com>
2018-05-14 15:25:53 -0500

    added sorting by therapist server side

--- server/controllers/outreachCtrl.js
+++ server/controllers/outreachCtrl.js
@@ -153,3 +153,42 @@ module.exports.sortByInquiryDate = (req,res,next) => {
   })
 
 }
+
+module.exports.sortByTherapist = (req,res,next) => {
+  let {Client, Substance, User} = req.app.get('models');
+  Client.findAll({include:[Substance, 'therapist'], order:[['therapist_id' , 'DESC']]})
+  .then(clients=>{
+    console.log('clients', clients);
+    
+
+    const clientObj = clients.map(client => {
+      return Object.assign(
+        {},
+        {
+          first_name: client.first_name,
+          last_name: client.last_name,
+          gender: client.gender,
+          dob: client.dob,
+          frequency: client.frequency,
+          last_use: client.last_use,
+          length_of_use: client.length_of_use,
+          previous_treatment: client.previous_treatment,
+          mental_health: client.mental_health,
+          si_hi: client.si_hi,
+          progress: client.progress,
+          severity: client.severity,
+          substancesUsed: client.Substances.map(subs => {
+              return subs.name.charAt(0).toUpperCase() + subs.name.slice(1)
+          }),
+          therapist: client.therapist.username,
+          inquiry: client.createdAt
+        }
+      )
+    
+      
+    })
+    
+    res.status(200).json(clientObj);
+  })
+
+}
--- server/routes/outreach.js
+++ server/routes/outreach.js
@@ -3,11 +3,12 @@
 const { Router } = require("express");
 const router = Router();
 
-const {getAllClients, sortBySeverity, sortByGender, sortByInquiryDate} = require('../controllers/outreachCtrl');
+const {getAllClients, sortBySeverity, sortByGender, sortByInquiryDate, sortByTherapist} = require('../controllers/outreachCtrl');
 
 router.get("/server/outreach", getAllClients);
 router.get('/server/sortBySeverity', sortBySeverity)
 router.get('/server/sortByGender', sortByGender)
 router.get('/server/sortByInquiryDate', sortByInquiryDate)
+router.get('/server/sortByTherapist', sortByTherapist)
 
 module.exports = router;
